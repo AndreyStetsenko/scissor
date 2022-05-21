2ecd108077d0eea80ab4b8c5a434ff7300402739
AndreyStetsenko <andrey@stetsenko.org>
2022-05-22 01:27:27 +0300

    Initial commit

---
+++ assets/javascript/src/modules/header/index.js
@@ -0,0 +1,14 @@
+import helpers from './modules/helpers';
+import VideoBanner from './modules/videoBanner';
+import lazyload from './modules/lazyload';
+
+export default function header() {
+    helpers();
+    new VideoBanner();
+    lazyload();
+
+    window.addEventListener('error', (e) => {
+        const { message, filename, lineno, colno, error } = e;
+        console.log([message, filename, lineno, colno, error]);
+    });
+}
---
+++ assets/javascript/src/pages/home/index.js
@@ -0,0 +1,23 @@
+import '@popperjs/core';
+import 'bootstrap';
+import header from '../../modules/header';
+
+/**
+ * Static page
+ */
+class Static {
+    /**
+     * Static page constructor
+     */
+    constructor() {
+        this.initModules();
+    }
+    /**
+     * Static for init plugins
+     */
+    initModules() {
+        header();
+    }
+}
+
+new Static();
